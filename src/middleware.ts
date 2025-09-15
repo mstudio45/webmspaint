34fae4635e40bfc525146aadcb652ed0f0a9518c
upio <75510171+notpoiu@users.noreply.github.com>
2025-09-15 01:08:06 -0400

    feat: added redirection for registry

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,21 @@
-export { auth as middleware } from "@/auth";
+import { NextRequest, NextResponse } from "next/server";
+import { auth } from "./auth";
+
+export default async function middleware(req: NextRequest) {
+  await auth();
+
+  // Registry redirection
+  if (
+    req.nextUrl.pathname.startsWith("/r/") &&
+    !req.nextUrl.pathname.endsWith(".json")
+  ) {
+    const url = req.nextUrl.clone();
+    url.pathname = `${url.pathname}.json`;
+    return NextResponse.redirect(url);
+  }
+
+  return NextResponse.next();
+}
 
 export const config = {
   matcher: [
