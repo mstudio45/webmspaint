37e7a901bdc2105c92d9d006a95272eafe6d51a0
upio <75510171+notpoiu@users.noreply.github.com>
2025-09-15 01:42:53 -0400

    fix: fixed huge spacing on edge cases

--- src/components/obsidian/elements/Divider.tsx
+++ src/components/obsidian/elements/Divider.tsx
@@ -1,3 +1,5 @@
 export default function Divider() {
-    return <hr className="w-full h-[4px] border-[rgb(40,40,40)] border" />
+  return (
+    <hr className="w-full h-[4px] border-[rgb(40,40,40)] border mt-0 mb-0" />
+  );
 }
