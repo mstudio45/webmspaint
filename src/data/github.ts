5fc34ea727a3b5bf2b83f80f35aca66f2f034ff4
mstudio45 <69036065+mstudio45@users.noreply.github.com>
2025-10-25 21:12:11 +0200

    feat: forgot one repo

--- src/data/github.ts
+++ src/data/github.ts
@@ -2,5 +2,5 @@ export const repoWhitelist: { [key: string]: string[] } = {
     "mspaint-cc": ["assets", "translations"],
     "deividcomsono": ["lucide-roblox-direct", "Obsidian"],
     "mstudio45": ["MSESP", "LinoriaLib"],
-    "notpoiu": ["RBXL-HTML-Parser"]
+    "notpoiu": ["RBXL-HTML-Parser", "Scripts"]
 };
