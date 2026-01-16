e467647ebbab9a7745fec065f0886731110bb8e2
upio <75510171+notpoiu@users.noreply.github.com>
2026-01-16 11:44:25 -0500

    feat: add mr tuffvid

--- src/server/authutils.ts
+++ src/server/authutils.ts
@@ -6,7 +6,7 @@ export async function userDiscordSignOut() {
     await signOut();
 }
 
-const allowed_users = ["1177722124035706931", "389792019360514048", "1098248637789786165"];
+const allowed_users = ["1177722124035706931", "389792019360514048", "1098248637789786165", "1397965462054240349"];
 export async function isUserAllowedOnDashboard(custom_user_id?: string) {
     let user_id = "1";
 
