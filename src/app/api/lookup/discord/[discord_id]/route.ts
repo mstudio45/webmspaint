699aeed235960215a9e91bcbd4281a9e63f73420
notpoiu <75510171+notpoiu@users.noreply.github.com>
2026-01-19 22:32:22 -0500

    fix: cors

--- src/app/api/lookup/discord/[discord_id]/route.ts
+++ src/app/api/lookup/discord/[discord_id]/route.ts
@@ -52,3 +52,14 @@ export async function GET(request: NextRequest, slug: { params: Promise<{ discor
         }
     });
 }
+
+export async function OPTIONS() {
+    return NextResponse.json({}, {
+        status: 200,
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+            "Access-Control-Allow-Headers": "Content-Type, Authorization"
+        }
+    });
+}
